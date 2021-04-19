332cf8929dd9f97c73efddabbe707178b31eb969
SetyMedia <setymedia@gmail.com>
2021-04-19 12:47:09 -0400

    Change page title in head for the home page

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Imsety Taylor - Motion Designer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
