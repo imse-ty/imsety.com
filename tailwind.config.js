b1c9ca8e6df93964ab4dfa0c38b1428b27a8a603
SetyMedia <setymedia@gmail.com>
2021-04-20 10:01:42 -0400

    Add fallback sans fonts for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      sans: ['Poppins']
+      sans: ['Poppins', 'ui-sans-serif', 'system-ui']
     },
     extend: {
       colors: {
