3a4e785ed2f254b53ddb586fbe2b30cd3c047aad
SetyMedia <setymedia@gmail.com>
2021-04-20 09:19:35 -0400

    Create config files for Tailwind

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <main>
+        <h1>Imsety Website</h1>
+      </main>
     </div>
   );
 }
