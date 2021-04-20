4d7f6b32d69ca89cf8d8729a2aed17bfbe77ae1d
SetyMedia <setymedia@gmail.com>
2021-04-20 09:53:31 -0400

    Add brand colors to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [`./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}`],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        imsetyBlack: '#131214',
+        imsetyWhite: '#EDEBEF'
+      }
+    }
   },
   variants: {
     extend: {}
