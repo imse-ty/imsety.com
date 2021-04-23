d27706b437153ee95d965e000ff9a46ca68664bb
SetyMedia <setymedia@gmail.com>
2021-04-23 15:45:53 -0400

    Make video clickable and add hover animation

--- components/video-reel-loop.js
+++ components/video-reel-loop.js
@@ -1,16 +1,18 @@
 export default function VideoReelLoop() {
   return (
     <>
-      <div className="absolute z-20 bg-imsetyWhite bg-opacity-25 min-w-full min-h-full" />
-      <video
-        autoPlay
-        loop
-        muted
-        className="absolute z-10 object-cover w-auto min-w-full min-h-full"
-      >
-        <source src="/setymedia-2020-reel-loop.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <a href="#">
+        <div className="absolute z-20 bg-imsetyWhite bg-opacity-25 min-w-full min-h-full transition-colors duration-300 hover:bg-opacity-0" />
+        <video
+          autoPlay
+          loop
+          muted
+          className="absolute z-10 object-cover w-auto min-w-full min-h-full"
+        >
+          <source src="/setymedia-2020-reel-loop.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </a>
     </>
   );
 }
