dc2e3bd8b61b947ead1091c01f71446ad33e6a42
SetyMedia <setymedia@gmail.com>
2021-04-28 08:49:58 -0400

    Add site metadata for SEO

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,12 @@ export default function Home() {
     <div className="text-imsetyBlack dark:text-imsetyWhite">
       <Head>
         <title>Imsety Taylor - Motion Designer</title>
+        <meta
+          name="description"
+          content="Imsety Taylor is a Motion Designer based in Atlanta, GA. He uses tools such as Adobe After Effects, Adobe Premiere Pro, and Cinema 4D."
+        />
+        <meta name="keywords" content="motion, design, motion design, art" />
+        <meta name="author" content="Imsety Taylor" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
