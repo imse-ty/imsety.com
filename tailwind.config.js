88d52e35acd840fe6253443d14b52429b181de29
SetyMedia <setymedia@gmail.com>
2021-06-05 11:37:42 -0400

    Add default padding for Tailwind CSS `container` class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '2rem',
+        md: '5rem'
+      }
+    },
     fontFamily: {
       sans: ['Poppins', 'ui-sans-serif', 'system-ui']
     },
