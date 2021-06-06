5bcbc2311e33c788c5d1147bc3e9582dd68f6edf
SetyMedia <setymedia@gmail.com>
2021-06-06 18:04:38 -0400

    Add `SocialMediaIcons` and site credits to `Footer` (#12)

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,10 @@
+import SocialMediaIcons from './social-media-icons';
+
 export default function Footer() {
   return (
     <footer>
+      <SocialMediaIcons />
+      <p className="text-sm">Site designed and developed my me 🙂</p>
       <p className="text-sm">© 2021 Imsety Taylor. All rights reserved.</p>
     </footer>
   );
