b80f36feabe5de33f9048d25fe8ea31879bb4e35
SetyMedia <setymedia@gmail.com>
2021-06-06 17:58:56 -0400

    Remove white shadow and enable dark mode for `backdropFilter` (#12)

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,15 +16,13 @@ module.exports = {
       colors: {
         imsetyBlack: '#131214',
         imsetyWhite: '#EDEBEF'
-      },
-      boxShadow: {
-        white:
-          '0 10px 30px -3px rgba(255, 255, 255, 0.1), 0 4px 8px -2px rgba(255, 255, 255, 0.05)'
       }
     }
   },
   variants: {
-    extend: { boxShadow: ['dark'] }
+    extend: {
+      backdropFilter: ['dark']
+    }
   },
   plugins: []
 };
