a95a812460601a47a70d1a6660f333fb496812cd
SetyMedia <setymedia@gmail.com>
2021-06-06 18:10:46 -0400

    Wrap `Footer` elements in container and add extra styling (#12)

--- components/footer.js
+++ components/footer.js
@@ -2,10 +2,18 @@ import SocialMediaIcons from './social-media-icons';
 
 export default function Footer() {
   return (
-    <footer>
-      <SocialMediaIcons />
-      <p className="text-sm">Site designed and developed my me 🙂</p>
-      <p className="text-sm">© 2021 Imsety Taylor. All rights reserved.</p>
+    <footer className="py-4">
+      <div className="container mx-auto">
+        <div className="my-3">
+          <SocialMediaIcons />
+        </div>
+        <p className="my-3 text-sm font-medium">
+          Site designed and developed my me 🙂
+        </p>
+        <p className="my-3 text-sm">
+          © 2021 Imsety Taylor. All rights reserved.
+        </p>
+      </div>
     </footer>
   );
 }
