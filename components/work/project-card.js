6932fe8e87e04a299d61e09f2f829e4bee6f2df6
SetyMedia <setymedia@gmail.com>
2021-06-07 20:45:56 -0400

    Make card label fade in whiled hovered with Framer Motion (#11)

--- components/work/project-card.js
+++ components/work/project-card.js
@@ -1,14 +1,23 @@
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 export default function ProjectCard() {
+  const labelVariants = { active: { opacity: 1 }, idle: { opacity: 0 } };
+
   return (
     <div className="relative w-full h-full text-imsetyWhite">
       <Link href="#">
         <a>
-          <div className="absolute flex flex-col justify-end w-full h-full p-4 md:p-8 bg-imsetyBlack dark:bg-imsetyWhite bg-opacity-20">
+          <motion.div
+            initial={false}
+            whileHover="active"
+            animate="idle"
+            variants={labelVariants}
+            className="absolute flex flex-col justify-end w-full h-full p-4 md:p-8 bg-imsetyBlack dark:bg-imsetyWhite bg-opacity-20"
+          >
             <h3 className="mb-3 text-2xl md:text-4xl font-bold">Lorem ipsum</h3>
             <p className="text-lg">Subtitle</p>
-          </div>
+          </motion.div>
           <img
             alt="Project cover image"
             src="https://source.unsplash.com/user/dayday95/500x500"
