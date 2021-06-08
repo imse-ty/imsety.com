edcc7a0352d6f983fea849694425a77c70f4b9ac
SetyMedia <setymedia@gmail.com>
2021-06-08 10:05:08 -0400

    Pass content props to `ProjectsSection` (#11)

--- components/work/projects-section.js
+++ components/work/projects-section.js
@@ -1,16 +1,14 @@
 import ProjectCard from './project-card';
 
-export default function ProjectsSection() {
-  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
+export default function ProjectsSection({ projects }) {
   return (
     <div className="sm:container grid grid-cols-2 gap-1 md:grid-cols-3 sm:gap-5">
-      {array.map((index) => {
+      {projects.map((project) => {
         return (
           <ProjectCard
-            key={index}
-            title="Lorem Ipsum"
-            subtitle="Subtitle"
+            key={project._id}
+            title={project.title}
+            subtitle={project.subtitle}
             coverSrc="https://source.unsplash.com/user/dayday95/640x640"
           />
         );
