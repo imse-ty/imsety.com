edfd6f531def59a0a7f9bb738617753246c14241
SetyMedia <setymedia@gmail.com>
2021-06-07 20:05:40 -0400

    Make hero heading a tad bit bigger for larger screens

--- components/work/hero.js
+++ components/work/hero.js
@@ -3,7 +3,7 @@ import SocialMediaIcons from '../social-media-icons';
 export default function Hero() {
   return (
     <div>
-      <h1 className="font-bold text-3xl md:text-4xl">
+      <h1 className="font-bold text-3xl md:text-5xl">
         Hey, my name is Imsety Taylor
       </h1>
       <p className="my-6 md:text-lg">
