801b0ee5b93493e2cd52222b8b81d4e94cb3b133
SetyMedia <setymedia@gmail.com>
2021-06-07 21:14:41 -0400

    Make `ProjectsSection` a grid layout (#11)

--- components/work/projects-section.js
+++ components/work/projects-section.js
@@ -2,7 +2,7 @@ import ProjectCard from './project-card';
 
 export default function ProjectsSection() {
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
       <ProjectCard />
       <ProjectCard />
       <ProjectCard />
