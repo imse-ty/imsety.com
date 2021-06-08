7417e740ac804a6e2c173c2b0263527c6cf8642d
SetyMedia <setymedia@gmail.com>
2021-06-07 20:58:00 -0400

    Give right to left hover animation to text (#11)

--- components/work/project-card.js
+++ components/work/project-card.js
@@ -3,20 +3,40 @@ import { motion } from 'framer-motion';
 
 export default function ProjectCard() {
   const labelVariants = { active: { opacity: 1 }, idle: { opacity: 0 } };
+  const textVariants = {
+    active: {
+      x: 0,
+      transition: { type: 'spring', duration: 0.6 }
+    },
+    idle: {
+      x: '-25%',
+      transition: { type: 'spring', duration: 0.6 }
+    }
+  };
 
   return (
-    <div className="relative w-full h-full text-imsetyWhite">
+    <div className="relative w-full h-full text-imsetyWhite overflow-hidden">
       <Link href="#">
         <a>
           <motion.div
             initial={false}
             whileHover="active"
             animate="idle"
+            transition={{
+              staggerChildren: 0.03
+            }}
             variants={labelVariants}
             className="absolute flex flex-col justify-end w-full h-full p-4 md:p-8 bg-imsetyBlack bg-opacity-40"
           >
-            <h3 className="mb-3 text-2xl md:text-4xl font-bold">Lorem ipsum</h3>
-            <p className="text-lg">Subtitle</p>
+            <motion.h3
+              variants={textVariants}
+              className="mb-3 text-2xl md:text-4xl font-bold"
+            >
+              Lorem ipsum
+            </motion.h3>
+            <motion.p variants={textVariants} className="text-lg">
+              Subtitle
+            </motion.p>
           </motion.div>
           <img
             alt="Project cover image"
