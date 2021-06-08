288294f4dc6d067d4c25d21b407e2140fdf01334
SetyMedia <setymedia@gmail.com>
2021-06-07 22:01:15 -0400

    Make `vh` Tailwind utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,17 @@ module.exports = {
     },
 
     extend: {
+      height: {
+        '10v': '10vh',
+        '20v': '20vh',
+        '30v': '30vh',
+        '40v': '40vh',
+        '50v': '50vh',
+        '60v': '60vh',
+        '70v': '70vh',
+        '80v': '80vh',
+        '90v': '90vh'
+      },
       colors: {
         imsetyBlack: '#131214',
         imsetyWhite: '#EDEBEF'
