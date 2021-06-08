97aa125f3bd212b21c3e43ef88ccaf3bb9f4c76b
SetyMedia <setymedia@gmail.com>
2021-06-07 20:34:58 -0400

    Add styling to `ProjectCard` (#11)

--- components/work/project-card.js
+++ components/work/project-card.js
@@ -1,15 +1,16 @@
 export default function ProjectCard() {
   return (
-    <div>
-      <div>
-        <h3>Lorem ipsum</h3>
-        <p>Subtitle</p>
+    <div className="relative w-full h-full text-imsetyWhite">
+      <div className="absolute flex flex-col justify-end w-full h-full p-4 md:p-8 bg-imsetyBlack dark:bg-imsetyWhite bg-opacity-20">
+        <h3 className="mb-3 text-2xl md:text-4xl font-bold">Lorem ipsum</h3>
+        <p className="text-lg">Subtitle</p>
       </div>
       <img
         alt="Project cover image"
         src="https://source.unsplash.com/user/dayday95/500x500"
         width={500}
         height={500}
+        className="w-full h-full"
       />
     </div>
   );
