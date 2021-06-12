1077a312dde64c5cf8adb59e44700cce6a80018d
SetyMedia <setymedia@gmail.com>
2021-06-12 14:25:46 -0400

    Add `Head` to info page (#19)

--- pages/info.js
+++ pages/info.js
@@ -1,3 +1,16 @@
+import Head from 'next/head';
+
 export default function Info() {
-  return <div>Info page</div>;
+  return (
+    <div>
+      <Head>
+        <title>Info - Imsety Taylor - Motion Designer</title>
+        <meta
+          name="description"
+          content="Imsety Taylor is a Motion Designer based in Atlanta, GA. He uses tools such as Adobe After Effects, Adobe Premiere Pro, and Cinema 4D."
+        />
+      </Head>
+      Info page
+    </div>
+  );
 }
