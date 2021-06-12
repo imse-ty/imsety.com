5b7a5435fbfb9dc8e8735603919af343c248dd64
SetyMedia <setymedia@gmail.com>
2021-06-12 18:27:25 -0400

    Create small screen Info page layout (#19)

--- pages/info.js
+++ pages/info.js
@@ -13,36 +13,35 @@ export default function Info() {
           content="Imsety Taylor is a Motion Designer based in Atlanta, GA. He uses tools such as Adobe After Effects, Adobe Premiere Pro, and Cinema 4D."
         />
       </Head>
-
       <Header />
-
-      <main className="h-70v mb-24">
-        <div>
+      <main className="h-screen mb-24">
+        <div className="h-1/2">
           <img
             alt="Imsety looking away in black and white"
             src="/imsety-looking-away-in-black-and-white.jpg"
             width="710"
             height="880"
-          />
-          <RightTrianglePattern
-            width="740"
-            viewBox="0 0 1008 1080"
-            className="hidden lg:block absolute overflow-hidden -right-96 h-full fill-current text-imsetyBlack dark:text-imsetyWhite opacity-5"
+            className="object-cover w-full h-full"
           />
         </div>
-        <div>
-          <h1>About</h1>
-          <p>
+        <div className="p-8 md:p-20 min-h-1/2 flex flex-col justify-center">
+          <h1 className="mb-6 font-bold text-3xl md:text-5xl">About</h1>
+          <p className="mb-6 font-medium md:text-lg">
             I'm a Motion Designer based in Atlanta, GA. When I'm designing and
             discovering new ideas with powerful tools such as After Effects or
             Cinema 4D, I regularly ask myself this question: "Is it pleasing to
             the eye?"
-            <br />
-            Want to get in contact? Email me: hello@imsety.com
           </p>
+          <a href="mailto:hello@imsety.com">
+            <button
+              type="button"
+              className="px-14 py-5 font-bold text-imsetyWhite dark:text-imsetyBlack bg-imsetyBlack dark:bg-imsetyWhite hover:scale-105 transform transition duration-300"
+            >
+              Reach Out
+            </button>
+          </a>
         </div>
       </main>
-
       <Footer />
     </div>
   );
