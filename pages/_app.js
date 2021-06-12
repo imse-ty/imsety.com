8d5cd84f0e2121403bebe33f12f287d2ed8baf9a
SetyMedia <setymedia@gmail.com>
2021-06-12 14:24:53 -0400

    Move root container and `Head` elements to `_app.js`

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import Head from 'next/head';
 import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="bg-imsetyWhite text-imsetyBlack dark:bg-imsetyBlack dark:text-imsetyWhite">
+      <Head>
+        <meta name="keywords" content="motion, design, motion design, art" />
+        <meta name="author" content="Imsety Taylor" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -10,20 +10,12 @@ import RightTrianglePattern from '../public/right-triangle-pattern.svg';
 
 export default function Home({ allProjects }) {
   return (
-    <div className="bg-imsetyWhite text-imsetyBlack dark:bg-imsetyBlack dark:text-imsetyWhite">
+    <div>
       <Head>
         <title>Imsety Taylor - Motion Designer</title>
         <meta
           name="description"
           content="Imsety Taylor is a Motion Designer based in Atlanta, GA. He uses tools such as Adobe After Effects, Adobe Premiere Pro, and Cinema 4D."
-        />
-        <meta name="keywords" content="motion, design, motion design, art" />
-        <meta name="author" content="Imsety Taylor" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
         />
       </Head>
       <Header />
