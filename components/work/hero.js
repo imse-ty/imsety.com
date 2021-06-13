f40c2a7275e151719b30e778e03ff302be665dcb
SetyMedia <setymedia@gmail.com>
2021-06-13 00:56:23 -0400

    Make top heading bigger

--- components/work/hero.js
+++ components/work/hero.js
@@ -3,7 +3,7 @@ import SocialMediaIcons from '../social-media-icons';
 export default function Hero() {
   return (
     <div>
-      <h1 className="font-bold text-3xl md:text-5xl">
+      <h1 className="font-bold text-4xl lg:text-6xl">
         Hey, my name is Imsety Taylor
       </h1>
       <p className="my-6 font-medium md:text-lg">
