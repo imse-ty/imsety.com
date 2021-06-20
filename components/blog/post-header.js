5947c8a8e21a42248348193d620b5a21e2364a6b
SetyMedia <setymedia@gmail.com>
2021-06-20 11:23:23 -0400

    Layout `PostHeader` (#35)

--- components/blog/post-header.js
+++ components/blog/post-header.js
@@ -1,3 +1,14 @@
-export default function PostHeader() {
-  return <h1>Post header</h1>;
+import DateFormatter from '../date-formatter';
+
+export default function PostHeader({ mainImage, title, date, readingTime }) {
+  return (
+    <header>
+      <img src={mainImage} />
+      <h1>{title}</h1>
+      <div className="my-2 text-xs tracking-widest text-gray-400">
+        <DateFormatter dateString={date} /> •{' '}
+        <time dateTime={`PTM${readingTime}`}>{readingTime} min</time> read
+      </div>
+    </header>
+  );
 }
