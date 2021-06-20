3cd4465aa16601fdd0b1ab39f768ec1c4c7d3220
SetyMedia <setymedia@gmail.com>
2021-06-19 23:20:29 -0400

    Add margin to `BlogPost` (#34)

--- components/blog/blog-post.js
+++ components/blog/blog-post.js
@@ -1,16 +1,17 @@
 export default function BlogPost() {
   return (
-    <div>
+    <div className="mb-8">
       <img
         alt="Blog post card cover image"
         src="https://source.unsplash.com/user/erondu/640x320"
         width={640}
         height={320}
+        className="mb-2"
       />
-      <div className="text-xs tracking-widest text-gray-400">
+      <div className="mb-2 text-xs tracking-widest text-gray-400">
         June 19, 2021 • 2 min read
       </div>
-      <h2 className="text-2xl font-bold">Lorem Ipsum</h2>
+      <h2 className="mb-2 text-2xl font-bold">Lorem Ipsum</h2>
       <p>Subtitle</p>
     </div>
   );
