d567c8fd7f8d20faac98124070f44363041c33f5
SetyMedia <setymedia@gmail.com>
2021-06-19 22:48:31 -0400

    Add `Head` to `Blog` (#34)

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,16 @@
+import Head from 'next/head';
 import BlogPost from '../components/blog/blog-post';
 
 export default function Blog() {
   return (
     <div>
+      <Head>
+        <title>Imsety Taylor - Motion Designer</title>
+        <meta
+          name="description"
+          content="Imsety Taylor is a Motion Designer based in Atlanta, GA. He uses tools such as Adobe After Effects, Adobe Premiere Pro, and Cinema 4D."
+        />
+      </Head>
       <BlogPost />
     </div>
   );
