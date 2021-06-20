23575c79d277f7ec43e634927b69943ac3121746
SetyMedia <setymedia@gmail.com>
2021-06-19 23:24:47 -0400

    Add props to `BlogPost` (#34)

--- components/blog/blog-post.js
+++ components/blog/blog-post.js
@@ -1,24 +1,40 @@
 import Link from 'next/link';
 
-export default function BlogPost() {
+export default function BlogPost({
+  title,
+  subtitle,
+  caption,
+  coverAlt,
+  coverImage,
+  href
+}) {
   return (
-    <Link href="#">
+    <Link href={href}>
       <a>
         <div className="mb-8">
           <img
-            alt="Blog post card cover image"
-            src="https://source.unsplash.com/user/erondu/640x320"
+            alt={coverAlt}
+            src={coverImage}
             width={640}
             height={320}
             className="mb-2"
           />
           <div className="mb-2 text-xs tracking-widest text-gray-400">
-            June 19, 2021 • 2 min read
+            {caption}
           </div>
-          <h2 className="mb-2 text-2xl font-bold">Lorem Ipsum</h2>
-          <p>Subtitle</p>
+          <h2 className="mb-2 text-2xl font-bold">{title}</h2>
+          <p>{subtitle}</p>
         </div>
       </a>
     </Link>
   );
 }
+
+BlogPost.defaultProps = {
+  title: 'Lorem Ipsum',
+  subtitle: 'Subtitle',
+  caption: 'June 19, 2021 • 2 min read',
+  coverAlt: 'Blog post cover image',
+  coverImage: 'https://source.unsplash.com/user/erondu/640x320',
+  href: '#'
+};
