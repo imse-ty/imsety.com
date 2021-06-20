a82ff576d269260d36d79ea1adedc4a814d839e1
SetyMedia <setymedia@gmail.com>
2021-06-19 23:16:45 -0400

    Layout `Blog` page (#34)

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,11 @@
 import Head from 'next/head';
+import Header from '../components/header';
+import Footer from '../components/footer';
 import BlogPost from '../components/blog/blog-post';
 
 export default function Blog() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Head>
         <title>Imsety Taylor - Motion Designer</title>
         <meta
@@ -11,7 +13,17 @@ export default function Blog() {
           content="Imsety Taylor is a Motion Designer based in Atlanta, GA. He uses tools such as Adobe After Effects, Adobe Premiere Pro, and Cinema 4D."
         />
       </Head>
-      <BlogPost />
+      <Header />
+      <main className="container flex-grow my-24">
+        <h1 className="mb-12 text-3xl md:text-5xl font-bold">Blog</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          <BlogPost />
+          <BlogPost />
+          <BlogPost />
+          <BlogPost />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
