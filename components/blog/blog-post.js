db0b35aa81dc59b7056ad8cd948172937afeea6d
SetyMedia <setymedia@gmail.com>
2021-06-19 23:20:55 -0400

    Make `BlogPost` clickable (#34)

--- components/blog/blog-post.js
+++ components/blog/blog-post.js
@@ -1,18 +1,24 @@
+import Link from 'next/link';
+
 export default function BlogPost() {
   return (
-    <div className="mb-8">
-      <img
-        alt="Blog post card cover image"
-        src="https://source.unsplash.com/user/erondu/640x320"
-        width={640}
-        height={320}
-        className="mb-2"
-      />
-      <div className="mb-2 text-xs tracking-widest text-gray-400">
-        June 19, 2021 • 2 min read
-      </div>
-      <h2 className="mb-2 text-2xl font-bold">Lorem Ipsum</h2>
-      <p>Subtitle</p>
-    </div>
+    <Link href="#">
+      <a>
+        <div className="mb-8">
+          <img
+            alt="Blog post card cover image"
+            src="https://source.unsplash.com/user/erondu/640x320"
+            width={640}
+            height={320}
+            className="mb-2"
+          />
+          <div className="mb-2 text-xs tracking-widest text-gray-400">
+            June 19, 2021 • 2 min read
+          </div>
+          <h2 className="mb-2 text-2xl font-bold">Lorem Ipsum</h2>
+          <p>Subtitle</p>
+        </div>
+      </a>
+    </Link>
   );
 }
