eef82645f224690f88ec224afa30eb8d2353f7a6
SetyMedia <setymedia@gmail.com>
2021-06-19 22:57:32 -0400

    Layout `BlogPost` component (#34)

--- components/blog/blog-post.js
+++ components/blog/blog-post.js
@@ -1,3 +1,17 @@
 export default function BlogPost() {
-  return <div>Blog post card</div>;
+  return (
+    <div>
+      <img
+        alt="Blog post card cover image"
+        src="https://source.unsplash.com/random/640x640"
+        width={640}
+        height={640}
+      />
+      <div className="text-xs tracking-widest text-gray-400">
+        June 19, 2021 • 2 min read
+      </div>
+      <h2 className="text-2xl font-bold">Lorem Ipsum</h2>
+      <p>Subtitle</p>
+    </div>
+  );
 }
