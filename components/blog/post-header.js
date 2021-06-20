cad591fbb02b8f430fe0dde8e8cc29490bfd3fc5
SetyMedia <setymedia@gmail.com>
2021-06-20 13:17:56 -0400

    Add `PostHeader` styles (#35)

--- components/blog/post-header.js
+++ components/blog/post-header.js
@@ -2,12 +2,16 @@ import DateFormatter from '../date-formatter';
 
 export default function PostHeader({ mainImage, title, date, readingTime }) {
   return (
-    <header className="mb-12">
-      <img src={mainImage} />
-      <h1>{title}</h1>
-      <div className="my-2 text-xs tracking-widest text-gray-400">
-        <DateFormatter dateString={date} /> •{' '}
-        <time dateTime={`PTM${readingTime}`}>{readingTime} min</time> read
+    <header className="flex flex-col md:flex-row items-center mb-12">
+      <div className="mb-8 md:w-1/2">
+        <img src={mainImage} className="w-full h-auto" />
+      </div>
+      <div className="container md:w-1/2">
+        <h1 className="text-3xl md:text-4xl xl:text-6xl font-bold">{title}</h1>
+        <div className="my-2 text-sm md:text-lg font-medium tracking-widest text-gray-400">
+          <DateFormatter dateString={date} /> •{' '}
+          <time dateTime={`PTM${readingTime}`}>{readingTime} min</time> read
+        </div>
       </div>
     </header>
   );
