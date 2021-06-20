28a6084ce9f4a1e3ca819dd33214df58f46deb1d
SetyMedia <setymedia@gmail.com>
2021-06-20 01:47:34 -0400

    Bring in real blog content from Sanity (#34)

--- components/blog/blog-post-section.js
+++ components/blog/blog-post-section.js
@@ -5,15 +5,16 @@ export default function BlogPostSection({ posts }) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
       {posts.map((post) => {
+        console.log(post.publishedDate);
         return (
           <BlogPost
             key={post._id}
             title={post.title}
             summary={post.summary}
-            date={post.publishedDate}
+            date={post.publishedAt}
             readingTime={post.estimatedReadingTime}
-            coverAlt={post.coverImage.alt}
-            coverImage={urlFor(post.coverImage).width(640).height(480).url()}
+            coverAlt={post.mainImage.alt}
+            coverImage={urlFor(post.mainImage).width(640).height(480).url()}
             href="#"
           />
         );
--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,11 @@
 import Head from 'next/head';
+import { getClient } from '../lib/sanity.server';
+import { indexPostQuery } from '../lib/queries';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import BlogPostSection from '../components/blog/blog-post-section';
 
-export default function Blog() {
+export default function Blog({ allPosts }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Head>
@@ -16,9 +18,17 @@ export default function Blog() {
       <Header />
       <main className="container flex-grow my-12 md:my-24">
         <h1 className="mb-12 text-3xl md:text-5xl font-bold">Blog</h1>
-        <BlogPostSection />
+        <BlogPostSection posts={allPosts} />
       </main>
       <Footer />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const allPosts = await getClient().fetch(indexPostQuery);
+
+  return {
+    props: { allPosts }
+  };
 }
