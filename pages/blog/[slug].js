f1b475f2df837e04761be7005c565ca59682cd46
SetyMedia <setymedia@gmail.com>
2021-06-20 11:49:36 -0400

    Bring in real content to `Post` (#35)

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,9 +1,28 @@
+import { useRouter } from 'next/router';
+import Error from 'next/error';
 import Head from 'next/head';
+import { urlFor, usePreviewSubscription } from '../../lib/sanity';
+import { getClient } from '../../lib/sanity.server';
+import { postQuery, postSlugsQuery } from '../../lib/queries';
 import Header from '../../components/header';
 import Footer from '../../components/footer';
 import PostHeader from '../../components/blog/post-header';
 
-export default function Post() {
+export default function Post({ data = {}, preview }) {
+  const router = useRouter();
+  const slug = data?.post?.slug;
+  const {
+    data: { post }
+  } = usePreviewSubscription(postQuery, {
+    params: { slug },
+    initialData: data,
+    enabled: preview && slug
+  });
+
+  if (!router.isFallback && !slug) {
+    return <Error statusCode={404} />;
+  }
+
   return (
     <div>
       <Head>
@@ -16,10 +35,37 @@ export default function Post() {
       <Header />
       <main>
         <article>
-          <PostHeader />
+          <PostHeader
+            mainImage={urlFor(post.mainImage).url()}
+            title={post.title}
+            date={post.publishedAt}
+            readingTime={post.estimatedReadingTime}
+          />
         </article>
       </main>
       <Footer />
     </div>
   );
+}
+
+export async function getStaticProps({ params, preview = false }) {
+  const post = await getClient().fetch(postQuery, {
+    slug: params.slug
+  });
+
+  return {
+    props: {
+      preview,
+      data: { post }
+    }
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = await getClient().fetch(postSlugsQuery);
+
+  return {
+    paths: paths.map((slug) => ({ params: { slug } })),
+    fallback: true
+  };
 }
