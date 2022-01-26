82bc405987c26984e26cae31bef4cacc7a46f04d
Imsety Taylor <imsety@imsety.com>
2022-01-25 22:28:13 -0500

    Implement Sanity content into project page (#36)

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,10 +1,35 @@
+import { useRouter } from 'next/router';
+import Error from 'next/error';
 import Head from 'next/head';
 import Image from 'next/image';
+import { serialize } from 'next-mdx-remote/serialize';
+import { MDXRemote } from 'next-mdx-remote';
+import { usePreviewSubscription } from '../../lib/sanity';
+import { getClient } from '../../lib/sanity.server';
+import { projectQuery, projectSlugsQuery } from '../../lib/queries';
 import Header from '../../components/header';
 import Footer from '../../components/footer';
 import ProjectHeader from '../../components/projects/project-header';
 
-export default function Project() {
+export default function Project({ data, source, preview }) {
+  const router = useRouter();
+  const slug = data?.post?.slug;
+
+  if (!router.isFallback && !slug) {
+    return <Error statusCode={404} />;
+  }
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  const { data: post } = usePreviewSubscription(projectQuery, {
+    params: { slug },
+    initialData: data.post,
+    enabled: preview && slug
+  });
+  const { title, summary, year, timeline, tools } = post;
+
   return (
     <div className="flex flex-col min-h-screen">
       <Head>
@@ -18,11 +43,11 @@ export default function Project() {
       <main className="container flex-grow my-12 md:my-24">
         <article>
           <ProjectHeader
-            title="400 Instagram Followers Piece"
-            summary='I gained 200, 300, and 400 followers in quick succession in only a couple of days. Because of that, I wanted to create an animation where the numbers "one-up" each other. As a way to illustrate what actually happened to my Instagram page.'
-            year="2020"
-            timeline="2 weeks"
-            tools="Cinema 4D, Redshift, After Effects"
+            title={title}
+            summary={summary}
+            year={year}
+            timeline={timeline}
+            tools={tools}
           />
           <div className="mb-12 md:mb-24 shadow-2xl">
             <Image
@@ -34,9 +59,36 @@ export default function Project() {
               objectFit="cover"
             />
           </div>
+          <div className="mx-auto prose prose-dark prose-lg dark:prose-light">
+            <MDXRemote {...source} />
+          </div>
         </article>
       </main>
       <Footer />
     </div>
   );
+}
+
+export async function getStaticProps({ params, preview = false }) {
+  const post = await getClient(preview).fetch(projectQuery, {
+    slug: params.slug
+  });
+  const mdxSource = await serialize(post.body);
+
+  return {
+    props: {
+      preview,
+      data: { post },
+      source: mdxSource
+    }
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = await getClient().fetch(projectSlugsQuery);
+
+  return {
+    paths: paths.map((slug) => ({ params: { slug } })),
+    fallback: true
+  };
 }
