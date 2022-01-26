e4bf615784e80e789a17665c7815d9c59836cf21
Imsety Taylor <imsety@imsety.com>
2022-01-26 10:05:37 -0500

    Fix `ProjectHeader` gap (#36)

--- components/projects/project-header.js
+++ components/projects/project-header.js
@@ -16,7 +16,7 @@ export default function ProjectHeader({
 }) {
   return (
     <header className="mb-12 md:mb-24">
-      <div className="flex flex-col xl:flex-row gap-6 md:gap-6 mb-6 md:mb-10">
+      <div className="flex flex-col xl:flex-row gap-6 md:gap-10 mb-6 md:mb-10">
         <div className="flex-1">
           <div className="w-16 h-2 bg-gray-500 mb-4 md:mb-6" />
           <h1 className="text-3xl md:text-6xl font-bold">{title}</h1>
