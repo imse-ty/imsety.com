70ac790fc477842686ebb14ace9f76f14ae38c20
Imsety Taylor <imsety@imsety.com>
2022-01-31 09:20:14 -0500

    Add changelog document (#47)

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -5,6 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 import project from './project';
 import post from './post';
+import changelog from './changelog';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -12,5 +13,5 @@ export default createSchema({
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([project, post])
+  types: schemaTypes.concat([project, post, changelog])
 });
