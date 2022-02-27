73dcf7b4f268f5c0913ef21931720c2963d170a3
Imsety Taylor <imsety@imsety.com>
2022-02-27 16:56:58 -0500

    Turn on Just-in-Time mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   corePlugins: {
