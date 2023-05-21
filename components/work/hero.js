a0aa350aa49fbc9c0929d05cc2ae7fb999dfcd42
Imsety <imsety@imsety.com>
2023-05-21 13:05:53 -0400

    Swap "Hey!" to "Greetings!"

--- components/work/hero.js
+++ components/work/hero.js
@@ -3,8 +3,8 @@ import SocialMediaIcons from '../social-media-icons';
 export default function Hero() {
   return (
     <div className="max-w-3xl">
-      <h1 className="mb-6 font-bold text-3xl xl:leading-tight md:text-4xl xl:text-6xl lg:pr-4">
-        Hey! My name is Imsety
+      <h1 className="mb-6 font-bold text-3xl xl:leading-tight md:text-4xl xl:text-5xl lg:pr-4">
+        Greetings! My name is&nbsp;Imsety
       </h1>
       <p className="mb-12">
         I'm a designer and developer based in Atlanta, GA. I leverage skills
