cd0d01df733df1a1b1a241bc7404d03546922d03
Imsety <imsety@imsety.com>
2023-05-21 13:07:03 -0400

    Make project section wider

--- components/work/projects-section.js
+++ components/work/projects-section.js
@@ -1,9 +1,13 @@
+import { motion } from 'framer-motion';
 import { urlFor } from '../../lib/sanity';
 import ProjectCard from './project-card';
 
 export default function ProjectsSection({ projects }) {
   return (
-    <div className="sm:container grid grid-cols-2 gap-1 md:grid-cols-3 sm:gap-5">
+    <motion.div
+      style={{ maxWidth: '2160px' }}
+      className="container grid grid-cols-2 gap-1 md:grid-cols-3 sm:gap-5"
+    >
       {projects.map((project) => {
         return (
           <ProjectCard
@@ -11,11 +15,11 @@ export default function ProjectsSection({ projects }) {
             title={project.title}
             subtitle={project.subtitle}
             coverAlt={project.coverImage.alt}
-            coverSrc={urlFor(project.coverImage).width(640).height(640).url()}
+            coverSrc={urlFor(project.coverImage).width(640).height(480).url()}
             href={`/projects/${project.slug}`}
           />
         );
       })}
-    </div>
+    </motion.div>
   );
 }
