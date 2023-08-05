16efcda05669c760ca65e3363f51030466193964
Imsety <imsety@imsety.com>
2023-08-05 18:14:25 -0400

    Fix footnote color and size

--- components/index-footer.js
+++ components/index-footer.js
@@ -2,7 +2,14 @@ import { Text } from 'krado-react';
 
 export default function IndexFooter() {
   return (
-    <Text variant="body.footnote">
+    <Text
+      variant="body.footnote"
+      sx={{
+        color: 'surface.regular',
+        fontWeight: 400,
+        fontSize: ['0.5em', 1]
+      }}
+    >
       Copyright © 2023 Imsety Taylor. All rights reserved.
     </Text>
   );
