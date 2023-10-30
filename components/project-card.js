bc02fe5f17b57c30b0c7483fec7b10d2cdf652e9
Imsety <imsety@imsety.com>
2023-10-30 10:36:32 -0400

    Make work page and project card

---
+++ components/project-card.js
@@ -0,0 +1,101 @@
+/** @jsxImportSource theme-ui */
+import { setyTheme } from '@/lib/site-theme';
+import { getColor } from '@theme-ui/color';
+import { motion } from 'framer-motion';
+import { Heading, theme } from 'krado-react';
+import { Flex } from 'theme-ui';
+
+export default function ProjectCard({ title, alt, src }) {
+  const boxVariants = {
+    active: {
+      scale: 0.97,
+      outline: `solid ${getColor(theme, 'surface.thin')} 15px`
+    }
+  };
+
+  const imageVariants = {
+    active: {
+      scale: 1.2
+    }
+  };
+
+  const labelVariants = {
+    active: {
+      x: '50px',
+      fontStretch: '125%',
+      fontWeight: 500
+    }
+  };
+
+  const bruh = {
+    active: {
+      left: 0,
+      opacity: 1
+    }
+  };
+
+  return (
+    <motion.div
+      initial="idle"
+      whileHover="active"
+      sx={{
+        width: '100%',
+        height: '100%',
+        position: 'relative'
+      }}
+    >
+      <motion.div
+        variants={boxVariants}
+        transition={{ type: 'spring', duration: 1 }}
+        sx={{
+          backgroundColor: 'secondary.light',
+          borderRadius: '24px',
+          width: '100%',
+          height: '500px',
+          position: 'relative',
+          overflow: 'hidden',
+          outline: `solid ${getColor(setyTheme, 'surface.thin')} 10px`
+        }}
+      >
+        <motion.img
+          variants={imageVariants}
+          transition={{ type: 'spring' }}
+          alt={alt}
+          src={src}
+          sx={{
+            width: '100%',
+            height: 'auto',
+            objectFit: 'cover'
+          }}
+        />
+      </motion.div>
+
+      <Flex sx={{ alignItems: 'center', position: 'relative' }}>
+        <motion.div
+          variants={bruh}
+          transition={{ type: 'spring', duration: 0.5 }}
+          sx={{
+            position: 'absolute',
+            borderRadius: 4,
+            backgroundColor: 'secondary.regular',
+            width: '30px',
+            height: '30px',
+            top: '38px',
+            left: -2,
+            opacity: 0
+          }}
+        />
+
+        <Heading
+          as={motion.h3}
+          variants={labelVariants}
+          transition={{ type: 'spring', duration: 0.5 }}
+          variant="display.h1"
+          sx={{ marginTop: 3 }}
+        >
+          {title}
+        </Heading>
+      </Flex>
+    </motion.div>
+  );
+}
--- pages/work.js
+++ pages/work.js
@@ -1,5 +1,48 @@
-import React from 'react';
+/** @jsxImportSource theme-ui */
+import Layout from '@/components/layout';
+import ProjectCard from '@/components/project-card';
+import { Container, Grid, Heading, theme } from 'krado-react';
 
 export default function Work() {
-  return <div>work</div>;
+  return (
+    <Layout>
+      <Container sx={{ height: '100vh' }}>
+        <Heading
+          variant="display.h1"
+          sx={{ marginBottom: 5, fontWeight: '500' }}
+        >
+          Projects
+        </Heading>
+        <Grid
+          sx={{
+            gap: 5,
+            gridTemplateColumns: '1fr 1fr',
+            width: '100%',
+            height: '75vh'
+          }}
+        >
+          <ProjectCard
+            title="Beloved Benefit"
+            alt="nah"
+            src="/20230618_020031.jpg"
+          />
+          <ProjectCard
+            title="Chick-fil-A"
+            alt="nah"
+            src="/20230824_081204.jpg"
+          />
+          <ProjectCard
+            title="Beeple Studios"
+            alt="nah"
+            src="/20230824_081204.jpg"
+          />
+          <ProjectCard
+            title="Keller Williams"
+            alt="nah"
+            src="/20230618_020031.jpg"
+          />
+        </Grid>
+      </Container>
+    </Layout>
+  );
 }
