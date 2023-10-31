42de3b6b9c1803b8c660a11435a2338b428fd4f8
Imsety <imsety@imsety.com>
2023-10-31 12:38:22 -0400

    Move theme to `_app.js`

--- components/layout.js
+++ components/layout.js
@@ -1,21 +1,17 @@
 'use client';
 
-import { ThemeUIProvider } from 'theme-ui';
 import { Box } from 'krado-react';
-import { setyTheme } from '@/lib/site-theme';
 
 export default function Layout({ children }) {
   return (
-    <ThemeUIProvider theme={setyTheme}>
-      <Box
-        as="main"
-        backgroundColor="background"
-        sx={{
-          height: '100vh'
-        }}
-      >
-        {children}
-      </Box>
-    </ThemeUIProvider>
+    <Box
+      as="main"
+      backgroundColor="background"
+      sx={{
+        height: '100vh'
+      }}
+    >
+      {children}
+    </Box>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,11 @@ import Head from 'next/head';
 import { ThemeUIProvider } from 'theme-ui';
 import { theme } from 'krado-react';
 import '../styles/globals.css';
+import { setyTheme } from '@/lib/site-theme';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <ThemeUIProvider theme={theme}>
+    <ThemeUIProvider theme={{ ...theme, ...setyTheme }}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
