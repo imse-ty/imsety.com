6c535d069f7bb22cc912397c6c77ab6becb2c059
Imsety <imsety@imsety.com>
2023-11-01 09:23:25 -0400

    Install `@next/mdx`

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const withMDX = require('@next/mdx')();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   async redirects() {
     return [
       {
@@ -105,4 +108,4 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
+module.exports = withMDX(nextConfig);
