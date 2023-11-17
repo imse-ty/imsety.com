76edefdc6c04445917810cbe1e300e4751c25906
Imsety <imsety@imsety.com>
2023-11-17 15:32:44 -0500

    Add cool cursor

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,63 @@
 'use client';
 
+import MouseTrail from '@pjsalita/react-mouse-trail';
 import { Box } from 'krado-react';
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
 export default function Layout({ children }) {
+  // const container = useRef();
+
+  // const circle = useRef();
+
+  // const manageMouseMove = (event) => {
+  //   const { clientX, clientY } = event;
+
+  //   const containerPosition = container.current.getBoundingClientRect();
+
+  //   const x = clientX - containerPosition.x;
+
+  //   const y = clientY - containerPosition.y;
+
+  //   circle.current.style.top = y + 'px';
+
+  //   circle.current.style.left = x + 'px';
+
+  //   draw(x, y);
+  // };
+
+  // const draw = (x, y) => {
+  //   const div = document.createElement('div');
+
+  //   div.classList.add('circle');
+
+  //   div.style.top = y + 'px';
+
+  //   div.style.left = x + 'px';
+
+  //   container.current.append(div);
+  // };
+
+  const config = {
+    color: '#000000',
+    idleAnimation: false,
+    idleAnimationCount: 10,
+    inverted: true,
+    size: 10,
+    trailCount: 20
+  };
+
   return (
     <Box
       as="main"
       backgroundColor="background"
       sx={{
-        height: '100vh'
+        height: '100vh',
+        cursor: 'none'
       }}
     >
+      {/* <div ref={circle} className="circle"></div> */}
+      <MouseTrail {...config} />
       {children}
     </Box>
   );
--- pages/work.js
+++ pages/work.js
@@ -13,123 +13,125 @@ export default function Work() {
   const [isVideoHidden, setIsVideoHidden] = useState(true);
 
   return (
-    <Container>
-      <Flex sx={{ flexDirection: 'column', alignItems: 'center', gap: 6 }}>
-        <Section
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
+    <Layout>
+      <Container>
+        <Flex sx={{ flexDirection: 'column', alignItems: 'center', gap: 6 }}>
+          <Section
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
 
-            marginTop: 6,
-            height: ['60vh']
-          }}
-        >
-          <Heading
-            variant="display.display"
-            sx={{
-              fontWeight: '600'
-            }}
-          >
-            Let&apos;s ◤ WORK together
-          </Heading>
-          <Text variant="body.summary" sx={{ marginBottom: [4, 5] }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </Text>
-          {!isVideoHidden && (
-            <FullScreenVideo onClick={() => setIsVideoHidden(true)} />
-          )}
-          <ReelVideo onClick={() => setIsVideoHidden(!isVideoHidden)} />
-        </Section>
-
-        {/* <section>
-          <Heading
-            variant="display.h1"
-            sx={{
-              marginBottom: 5,
-              fontSize: '128px',
-              fontWeight: '600',
-              fontStretch: '125%'
-            }}
-          >
-            Featured work
-          </Heading>
-          <Grid
-            sx={{
-              gap: 5,
-              gridTemplateColumns: ['1fr', null, '1fr 1fr'],
-              width: '100%'
-            }}
-          >
-            <ProjectCard
-              src="/20230618_020031.jpg"
-              title="Beloved Benefit"
-              href="/projects/hi"
-              layoutId="bruh"
-            />
-            <ProjectCard title="Chick-fil-A" href="/projects/hi" />
-            <ProjectCard title="Beeple Studios" href="/projects/hi" />
-            <ProjectCard title="Rock The Bells x Ford" href="/projects/hi" />
-            <ProjectCard title="Space and Time" href="/projects/hi" />
-            <ProjectCard title="OSOS" href="/projects/hi" />
-          </Grid>
-        </section>
-        <section>
-          <Grid
-            sx={{
-              gap: 3,
-
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              width: '100%',
-              height: '20vh',
-              gridTemplateColumns: '1fr 1fr'
+              marginTop: 6,
+              height: ['60vh']
             }}
           >
             <Heading
+              variant="display.display"
               sx={{
-                fontWeight: '600',
-                fontSize: '10vw',
-                width: '100%',
-                maxWidth: '50%',
-                fontStretch: '125%'
+                fontWeight: '600'
               }}
             >
-              Design
+              Let&apos;s ◤ WORK together
             </Heading>
-            <Box
-              sx={{
-                backgroundColor: 'red',
-                width: '100%',
-                height: '100%',
-                borderRadius: 4
-              }}
-            />
-          </Grid>
-          <Flex
-            sx={{
-              flexWrap: 'wrap',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              width: '100%',
-              height: '20vh'
-            }}
-          >
-            <Text sx={{ fontSize: 8, maxWidth: '50%' }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis
-              vehicula ipsum, nec consectetur justo.
+            <Text variant="body.summary" sx={{ marginBottom: [4, 5] }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             </Text>
+            {!isVideoHidden && (
+              <FullScreenVideo onClick={() => setIsVideoHidden(true)} />
+            )}
+            <ReelVideo onClick={() => setIsVideoHidden(!isVideoHidden)} />
+          </Section>
+
+          {/* <section>
             <Heading
+              variant="display.h1"
               sx={{
+                marginBottom: 5,
+                fontSize: '128px',
                 fontWeight: '600',
-                fontSize: '10vw',
                 fontStretch: '125%'
               }}
             >
-              + code
+              Featured work
             </Heading>
-          </Flex>
-        </section> */}
-      </Flex>
-    </Container>
+            <Grid
+              sx={{
+                gap: 5,
+                gridTemplateColumns: ['1fr', null, '1fr 1fr'],
+                width: '100%'
+              }}
+            >
+              <ProjectCard
+                src="/20230618_020031.jpg"
+                title="Beloved Benefit"
+                href="/projects/hi"
+                layoutId="bruh"
+              />
+              <ProjectCard title="Chick-fil-A" href="/projects/hi" />
+              <ProjectCard title="Beeple Studios" href="/projects/hi" />
+              <ProjectCard title="Rock The Bells x Ford" href="/projects/hi" />
+              <ProjectCard title="Space and Time" href="/projects/hi" />
+              <ProjectCard title="OSOS" href="/projects/hi" />
+            </Grid>
+          </section>
+          <section>
+            <Grid
+              sx={{
+                gap: 3,
+
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                width: '100%',
+                height: '20vh',
+                gridTemplateColumns: '1fr 1fr'
+              }}
+            >
+              <Heading
+                sx={{
+                  fontWeight: '600',
+                  fontSize: '10vw',
+                  width: '100%',
+                  maxWidth: '50%',
+                  fontStretch: '125%'
+                }}
+              >
+                Design
+              </Heading>
+              <Box
+                sx={{
+                  backgroundColor: 'red',
+                  width: '100%',
+                  height: '100%',
+                  borderRadius: 4
+                }}
+              />
+            </Grid>
+            <Flex
+              sx={{
+                flexWrap: 'wrap',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                width: '100%',
+                height: '20vh'
+              }}
+            >
+              <Text sx={{ fontSize: 8, maxWidth: '50%' }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mattis
+                vehicula ipsum, nec consectetur justo.
+              </Text>
+              <Heading
+                sx={{
+                  fontWeight: '600',
+                  fontSize: '10vw',
+                  fontStretch: '125%'
+                }}
+              >
+                + code
+              </Heading>
+            </Flex>
+          </section> */}
+        </Flex>
+      </Container>
+    </Layout>
   );
 }
