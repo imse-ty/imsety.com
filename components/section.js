9912c095746d08030d95898bdf5bde3cdec5d01a
Imsety <imsety@imsety.com>
2023-11-18 07:01:56 -0500

    Make sections full width for a sec

--- components/section.js
+++ components/section.js
@@ -1,5 +1,5 @@
 import { Box } from 'krado-react';
 
 export default function Section({ ...rest }) {
-  return <Box sx={{ width: '100%', maxWidth: '1336px' }} {...rest} />;
+  return <Box sx={{ width: '100%' }} {...rest} />;
 }
