039211875cdf192bce6ac8735fdaa99c615993b3
Imsety <imsety@imsety.com>
2023-11-22 07:18:47 -0500

    Tidy shade a little bit

--- components/shade.js
+++ components/shade.js
@@ -1,12 +1,24 @@
 /** @jsxImportSource theme-ui */
 
 import { motion } from 'framer-motion';
-import { Button, Flex } from 'krado-react';
+import {
+  Box,
+  Button,
+  Flex,
+  Footer,
+  FooterContent,
+  FooterFootnote,
+  FooterItem,
+  ToggleIcon
+} from 'krado-react';
 import { useState } from 'react';
 import { getColor } from '@theme-ui/color';
 import { setyTheme } from '@/lib/site-theme';
 import Link from 'next/link';
 import Hero from '@/components/hero';
+import IndexHeader from './index-header';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useColorMode } from 'theme-ui';
 
 export function CTAButton({ href, children, variant }) {
   const buttonVariants = {
@@ -20,9 +32,9 @@ export function CTAButton({ href, children, variant }) {
         variants={buttonVariants}
         initial="hidden"
         animate="visible"
+        size="small"
         transition={{ type: 'spring', delay: 0.7, duration: 2 }}
         variant={variant}
-        size="small"
         sx={{
           borderRadius: 2,
           fontWeight: 500
@@ -35,6 +47,7 @@ export function CTAButton({ href, children, variant }) {
 }
 
 export default function Shade() {
+  const [colorMode, setColorMode] = useColorMode('dark');
   const [isCovered, setIsCovered] = useState(true);
 
   const shadeVariants = {
@@ -43,18 +56,23 @@ export default function Shade() {
     },
     hide: {
       clipPath: `polygon(0 0%, 6% 0, 0 6%, 0 0)`
-    },
-    hover: {
-      backgroundColor: getColor(setyTheme, 'secondary.regular')
+    }
+  };
+
+  const backgroundVariants = {
+    hide: {
+      backgroundColor: getColor(setyTheme, 'primary.contrast')
     }
   };
 
   const contentVariants = {
     show: {
+      opacity: 1,
       scale: 1
     },
     hide: {
-      scale: 3
+      opacity: 0,
+      scale: 1.1
     }
   };
   return (
@@ -63,28 +81,32 @@ export default function Shade() {
       variants={shadeVariants}
       animate={isCovered ? 'show' : 'hide'}
       whileHover={isCovered ? '' : 'hover'}
-      transition={{ type: 'spring', bounce: 0.2 }}
+      transition={{ type: 'spring', bounce: 0.2, duration: 0.8 }}
       onTap={() => setIsCovered(!isCovered)}
       sx={{
         position: 'fixed',
         width: '100vw',
         height: '100vh',
-        color: 'primary.contrast',
-        backgroundColor: 'rgba(232,232,232,0.97)',
         zIndex: 5,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        flexDirection: 'column'
       }}
     >
+      <IndexHeader />
       <Flex
         as={motion.div}
         variants={contentVariants}
+        initial="hide"
         animate={isCovered ? 'show' : 'hide'}
         transition={{ type: 'spring', duration: 1 }}
         sx={{
           flexDirection: 'column',
           alignItems: 'center',
-          gap: 3,
+          gap: [3, 4],
+          justifyContent: 'center',
+          height: '100%',
+
           textAlign: 'center'
         }}
       >
@@ -96,6 +118,42 @@ export default function Shade() {
           </CTAButton>
         </Flex>
       </Flex>
+      <Footer>
+        <FooterFootnote>
+          Copyright © 2023 Imsety LLC. All rights reserved.
+        </FooterFootnote>
+        <FooterContent>
+          <Flex sx={{ alignItems: 'center' }}>
+            <FooterItem href="#">Contact</FooterItem>
+            <FooterItem href="#">About</FooterItem>
+            <ToggleIcon
+              onClick={(e) => {
+                setColorMode(colorMode === 'default' ? 'dark' : 'default');
+              }}
+              sx={{
+                fontSize: 3,
+                marginLeft: 4,
+                color: 'text.secondary'
+              }}
+            >
+              {colorMode === 'default' ? <FaMoon /> : <FaSun />}
+            </ToggleIcon>
+          </Flex>
+        </FooterContent>
+      </Footer>
+      <motion.div
+        variants={backgroundVariants}
+        animate={isCovered ? 'show' : 'hide'}
+        sx={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          backgroundColor: 'background',
+          // backdropFilter: 'blur(30px)',
+          opacity: 0.98,
+          zIndex: -1
+        }}
+      />
     </Flex>
   );
 }
