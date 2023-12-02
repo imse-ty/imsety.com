f4c513c4b92ae815c7c00f07fc65d233e95d0a44
Imsety <imsety@imsety.com>
2023-12-02 16:48:49 -0500

    Uninstall MDX and mouse trail stuff

--- components/layout.js
+++ components/layout.js
@@ -1,52 +1,8 @@
 'use client';
 
-import MouseTrail from '@pjsalita/react-mouse-trail';
 import { Box } from 'krado-react';
-import { useRef } from 'react';
-import { useEffect } from 'react';
 
 export default function Layout({ children, disableScroll }) {
-  // const container = useRef();
-
-  // const circle = useRef();
-
-  // const manageMouseMove = (event) => {
-  //   const { clientX, clientY } = event;
-
-  //   const containerPosition = container.current.getBoundingClientRect();
-
-  //   const x = clientX - containerPosition.x;
-
-  //   const y = clientY - containerPosition.y;
-
-  //   circle.current.style.top = y + 'px';
-
-  //   circle.current.style.left = x + 'px';
-
-  //   draw(x, y);
-  // };
-
-  // const draw = (x, y) => {
-  //   const div = document.createElement('div');
-
-  //   div.classList.add('circle');
-
-  //   div.style.top = y + 'px';
-
-  //   div.style.left = x + 'px';
-
-  //   container.current.append(div);
-  // };
-
-  const config = {
-    color: '#000000',
-    idleAnimation: false,
-    idleAnimationCount: 10,
-    inverted: true,
-    size: 10,
-    trailCount: 20
-  };
-
   return (
     <Box
       as="main"
@@ -57,8 +13,6 @@ export default function Layout({ children, disableScroll }) {
         overflow: disableScroll ? 'hidden' : 'initial'
       }}
     >
-      {/* <div ref={circle} className="circle"></div> */}
-      {/* <MouseTrail {...config} /> */}
       {children}
     </Box>
   );
