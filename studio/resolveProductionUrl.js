fae807ec5ae62583419d84ce8f5dae8e10d538a3
Imsety <imsety@imsety.com>
2023-12-27 21:04:58 -0500

    Prepare for v2 rewrite

--- studio/resolveProductionUrl.js
+++
@@ -1,8 +0,0 @@
-import { constants } from './constants/constants';
-
-const previewSecret = process.env.SANITY_STUDIO_PREVIEW_SECRET;
-const projectUrl = constants.siteUrl;
-
-export default function resolveProductionUrl(document) {
-  return `${projectUrl}/api/preview-${document._type}?secret=${previewSecret}&slug=${document.slug.current}`;
-}
