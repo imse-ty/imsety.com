851c79e0fe144c1f41122b536e06cc97b2bdf176
Imsety <imsety@imsety.com>
2023-12-30 08:26:46 -0500

    Remove height in Layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,9 +5,7 @@ import { Box } from 'krado-react';
 export default function Layout({ children, disableScroll }) {
   return (
     <Box
-      backgroundColor='background'
       sx={{
-        height: '100vh',
         position: 'relative',
         overflow: disableScroll ? 'hidden' : 'initial'
       }}
