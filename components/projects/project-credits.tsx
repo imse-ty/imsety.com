d1f0feefc17e149795eaeee4583f8ac8729725a5
Imsety <imsety@imsety.com>
2024-01-27 21:56:25 -0500

    Add confetti to credits

--- components/projects/project-credits.tsx
+++ components/projects/project-credits.tsx
@@ -1,6 +1,12 @@
-import { Box, Flex, Grid } from 'krado-react';
+/** @jsxImportSource theme-ui */
+// @ts-nocheck
+
+import { Flex, Grid, colors } from 'krado-react';
 import Heading from '../fixed-krado-components/Heading';
 import Text from '../fixed-krado-components/Text';
+import { useReward } from 'react-rewards';
+import { useEffect, useRef } from 'react';
+import { useInView } from 'framer-motion';
 
 function Card({ children, ...rest }) {
   return (
@@ -10,7 +16,7 @@ function Card({ children, ...rest }) {
         alignItems: 'center',
         textAlign: 'center',
         gap: 4,
-
+        position: 'relative',
         marginY: 4,
         marginX: [0, null, null, null, '-50%'],
         padding: [4, 6],
@@ -45,36 +51,70 @@ function Mention() {
 }
 
 export default function ProjectCredits() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { amount: 'all' });
+  const { reward } = useReward('rewardId', 'confetti', {
+    lifetime: 500,
+    spread: 100,
+    colors: [
+      colors.nut400,
+      colors.ptah300,
+      colors.usir300,
+      colors.sekmet300,
+      colors.oshun300
+    ],
+    position: 'absolute',
+    elementSize: 20
+  });
+
+  useEffect(() => {
+    reward();
+  }, [isInView]);
+
   return (
-    <Card>
-      <Flex
-        sx={{
-          flexDirection: 'column',
-          gap: 3,
-          marginBottom: 5,
-          maxWidth: '900px'
-        }}
-      >
-        <Heading variant='display.h2' sx={{ color: 'surface.thin' }}>
-          Credits
-        </Heading>
-        <Text variant='body.summary' sx={{ flex: 1, color: 'surface.thin' }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-          facilisis quam ex, eu vehicula justo sollicitudin pharetra.
-        </Text>
-      </Flex>
-      <Grid
-        sx={{
-          gridTemplateColumns: '1fr 1fr',
-          gap: 6,
-          flex: 1
-        }}
-      >
-        <Mention />
-        <Mention />
-        <Mention />
-        <Mention />
-      </Grid>
-    </Card>
+    <div ref={ref}>
+      <Card>
+        <span
+          style={{
+            pointerEvents: 'none',
+            position: 'absolute',
+            overflow: 'hidden',
+            width: '100%',
+            height: '100%',
+            top: 0,
+            bottom: 0
+          }}
+          id='rewardId'
+        />
+        <Flex
+          sx={{
+            flexDirection: 'column',
+            gap: 3,
+            marginBottom: 5,
+            maxWidth: '900px'
+          }}
+        >
+          <Heading variant='display.h2' sx={{ color: 'surface.thin' }}>
+            Credits
+          </Heading>
+          <Text variant='body.summary' sx={{ flex: 1, color: 'surface.thin' }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+            facilisis quam ex, eu vehicula justo sollicitudin pharetra.
+          </Text>
+        </Flex>
+        <Grid
+          sx={{
+            gridTemplateColumns: '1fr 1fr',
+            gap: 6,
+            flex: 1
+          }}
+        >
+          <Mention />
+          <Mention />
+          <Mention />
+          <Mention />
+        </Grid>
+      </Card>
+    </div>
   );
 }
