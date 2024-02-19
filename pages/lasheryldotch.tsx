dae887f62c586621db0e75855c79cdc0d646d3f1
Imsety <imsety@imsety.com>
2024-02-19 10:55:35 -0500

    Make new page

---
+++ pages/lasheryldotch.tsx
@@ -0,0 +1,30 @@
+/** @jsxImportSource theme-ui */
+//@ts-nocheck
+
+import VideoSection from '@/components/video-section';
+import { useState } from 'react';
+import { NextSeo } from 'next-seo';
+
+export default function Reel() {
+  const [isVideoActive, setIsVideoActive] = useState(false);
+
+  return (
+    <>
+      <NextSeo noindex />
+      <VideoSection
+        isPlayButtonHidden={false}
+        isVideoActive={isVideoActive}
+        url='https://vimeo.com/910900048?share=copy'
+        previewSrc='walter-preview.webm'
+        poster='walter-thumbnail.jpg'
+        setIsVideoActive={() => {
+          if (typeof umami !== 'undefined' && !isVideoActive) {
+            umami.track('lasheryldotch-blurb-play');
+          }
+
+          setIsVideoActive(!isVideoActive);
+        }}
+      />
+    </>
+  );
+}
